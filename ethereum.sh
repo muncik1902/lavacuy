2f4dd997b30c76211b8953586d2a4511a5c44500
muncik1902 <49601001+muncik1902@users.noreply.github.com>
2024-06-13 13:26:56 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-87f8cb59-c1ca-4750-a7ee-c979df08d1fb/"
 
 OUTPUT_DIR="logs"
 
